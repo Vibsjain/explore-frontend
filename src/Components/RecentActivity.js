5b35896516a44f06dcb0e4bdcf15a411ffb55d75
Vaibhav Gupta <guptavaibhav432@gmail.com>
2022-11-17 13:48:05 +0530

    See More Activity

--- src/Components/RecentActivity.js
+++ src/Components/RecentActivity.js
@@ -1,9 +1,107 @@
+import React, { useState } from "react";
 import ActivityCard from "./ActivityCard";
 import Avator from "../Assets/Images/avator.png";
 import homeTop from "../Assets/Images/homeTop.png";
 
 function RecentActivity() {
+    const [extend, setExtend] = useState(false);
     const data = [
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
+        {
+            userImage: Avator,
+            userName: "Abhinav",
+            restImage: homeTop,
+            restName: "Krishna Cafe",
+            star: 4,
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.",
+        },
         {
             userImage: Avator,
             userName: "Abhinav",
@@ -42,18 +140,52 @@ function RecentActivity() {
             <p className="text-[45px] font-semibold py-10 z-[1]">
                 Recent Activities
             </p>
-            <div className="flex flex-wrap justify-evenly my-8">
-                {data.map((item) => (
-                    <ActivityCard
-                        userImage={item.userImage}
-                        userName={item.userName}
-                        restImage={item.restImage}
-                        restName={item.restName}
-                        star={item.star}
-                        review={item.review}
-                    />
-                ))}
-            </div>
+            {!extend ? (
+                <div className="flex flex-wrap justify-evenly my-4">
+                    {data.map((item) => (
+                        <ActivityCard
+                            userImage={item.userImage}
+                            userName={item.userName}
+                            restImage={item.restImage}
+                            restName={item.restName}
+                            star={item.star}
+                            review={item.review}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <div className="flex flex-wrap justify-evenly my-4">
+                    {data.slice(0, 6).map((item) => (
+                        <ActivityCard
+                            userImage={item.userImage}
+                            userName={item.userName}
+                            restImage={item.restImage}
+                            restName={item.restName}
+                            star={item.star}
+                            review={item.review}
+                        />
+                    ))}
+                </div>
+            )}
+            {extend ? (
+                <div className="my-4">
+                    <button
+                        className="text-[24px] text-[#3A3A3A]"
+                        onClikc={() => setExtend(!extend)}
+                    >
+                        See More Activity
+                    </button>
+                </div>
+            ) : (
+                <div className="my-4">
+                    <button
+                        className="text-[24px] text-[#3A3A3A]"
+                        onClikc={() => setExtend(!extend)}
+                    >
+                        See Less Activity
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
